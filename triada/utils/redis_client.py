022db73725d0beafd24af40b0195843c2851a2cb
Swertik <herobrin664@gmail.com>
2025-03-06 02:25:32 +0300

    Hotfix6

--- triada/utils/redis_client.py
+++ triada/utils/redis_client.py
@@ -1,3 +1,4 @@
 import redis.asyncio as redis  # Вместо aioredis
+from triada.config.settings import REDIS_PASSWORD
 
-redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+redis_client = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True, password=REDIS_PASSWORD)
